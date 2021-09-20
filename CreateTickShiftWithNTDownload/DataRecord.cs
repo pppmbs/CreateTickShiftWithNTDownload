63d32fc495fb54a152a7f66fb482a4a2832addf3
pppmbs <pppmbs@gmail.com>
2021-09-19 20:21:33 -0500

    This program takes the NT export tick data and shift 200 ticks and generate 9 additional augmented training data

---
+++ CreateTickShiftWithNTDownload/DataRecord.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreateTickShiftWithNTDownload
+{
+    class DataRecord
+    {
+        /*
+        public String CommonName { get; set; }
+        public String FormalName { get; set; }
+        public String TelephoneCode { get; set; }
+        public String CountryCode { get; set; }
+        */
+
+        public String Date { get; set; }
+
+        public String Time { get; set; }
+
+        public String Fraction { get; set; }
+
+        public String Last { get; set; }
+
+        public String Bid { get; set; }
+
+        public String Ask { get; set; }
+
+        public String Volume { get; set; }
+
+    }
+}
--- CreateTickShiftWithNTDownload/Program.cs
+++ CreateTickShiftWithNTDownload/Program.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using CsvHelper;
+using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +12,118 @@ namespace CreateTickShiftWithNTDownload
 {
     class Program
     {
+        static class Constants
+        {
+            public const int TickCount = 2000; // ticks per bar
+            //public const int barsLookAhear = 5; // look ahead 5 bars
+            //public const int minBarRecords = 50; //anything less Some indicators, e.g. SMA50, MACD, will not have any value
+            public const int slidingWindow = TickCount / 10; // sliding window to create multiple variations of the 2000 ticks bar records, i.e. expended the data size by 10x
+                                                             //public const int slidingWindow = 97; // largest prime <100 sliding window to create multiple variations of the 2000 ticks bar records, i.e. expended the data size by 10x
+            public const int slidingTotal = 10; // total number of sliding, i.e. # of augmented files generated
+        }
+
+        static public List<String> SplitESFileIntoDailyDataFiles(String esFile)
+        {
+            List<String> dailyDataFiles = new List<string>();
+            string outputFileName;
+
+            using (var sr = new StreamReader(esFile))
+            {
+                var reader = new CsvReader(sr, CultureInfo.InvariantCulture);
+
+                //CSVReader will now read the entire es file into an enumerable
+                IEnumerable records = reader.GetRecords<DataRecord>().ToList();
+
+                List<DataRecord> listDataRecords = new List<DataRecord>();
+
+                String startDate = "";
+                bool startNewRecord = true;
+                foreach (DataRecord record in records)
+                {
+                    if (startNewRecord || record.Date.Contains(startDate))
+                    {
+                        if (startNewRecord)
+                        {
+                            listDataRecords = new List<DataRecord>();
+                            startNewRecord = false;
+                            startDate = record.Date;
+                        }
+
+                        listDataRecords.Add(record);
+                    }
+                    else
+                    {
+                        outputFileName = Path.GetFileNameWithoutExtension(esFile) + "-" + startDate + ".csv";
+                        using (var sw = new StreamWriter(outputFileName))
+                        {
+                            var writer = new CsvWriter(sw, CultureInfo.InvariantCulture);
+                            writer.WriteRecords(listDataRecords);
+                            writer.Flush();
+                        }
+                        dailyDataFiles.Add(outputFileName);
+                        startNewRecord = true;
+                    }
+                }
+                // flush last record
+                outputFileName = Path.GetFileNameWithoutExtension(esFile) + "-" + startDate + ".csv";
+                using (var sw = new StreamWriter(outputFileName))
+                {
+                    var writer = new CsvWriter(sw, CultureInfo.InvariantCulture);
+                    writer.WriteRecords(listDataRecords);
+                    writer.Flush();
+                }
+                dailyDataFiles.Add(outputFileName);
+            }
+            return dailyDataFiles;
+        }
+
         static void Main(string[] args)
         {
+            // To check the length of  
+            // Command line arguments   
+            if (args.Length == 0)
+            {
+                Console.WriteLine("CreateTickShiftWithNTDownload inputfiles");
+                Environment.Exit(0);
+            }
+
+            foreach (string inESFile in args)
+            {
+                // first spilt into daily files
+                List<String> dailyDataFiles = SplitESFileIntoDailyDataFiles(inESFile);
+                IEnumerable inFiles = dailyDataFiles;
+
+                // the slidingTotal decides how many versions of tick shift data files are there
+                for (int slidingNum = 1; slidingNum < Constants.slidingTotal; slidingNum++)
+                {
+                    string path = Constants.TickCount + "-NT-ticks-Shift\\" + Path.GetFileNameWithoutExtension(inESFile) + "." + slidingNum.ToString() + Path.GetExtension(inESFile);
+                    File.Delete(path);
+
+                    // for each daily file, shift the ticks (shifting window decided by slidingNum*slidingWindow) and write the shifted data into ONE file for each tick shift version
+                    foreach (String inFile in inFiles)
+                    {
+                        using (var sr = new StreamReader(inFile))
+                        {
+                            var reader = new CsvReader(sr, CultureInfo.InvariantCulture);
+                            List<DataRecord> ticks = new List<DataRecord>();
+
+                            //CSVReader will now read the whole file into an enumerable
+                            IEnumerable records = reader.GetRecords<DataRecord>().ToList();
+
+                            int skip = 1;
+                            foreach (DataRecord tick in records)
+                            {
+                                if (skip > slidingNum * Constants.slidingWindow)
+                                {
+                                    string bufString = tick.Date + " " + tick.Time + " " + tick.Fraction + ";" + tick.Bid + ";" + tick.Ask + ";" + tick.Volume;
+                                    File.AppendAllText(path, bufString + Environment.NewLine);
+                                }
+                                skip++;
+                            }
+                        }
+                    }
+                }
+            }
         }
     }
 }
